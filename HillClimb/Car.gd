5e2992dd81b8fd2309dda910f022588aeb9b9ce4
WheelOfPython <50963086+WheelOfPython@users.noreply.github.com>
2019-08-31 18:46:36 +0300

    dlt

--- HillClimb/Car.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		$front.angular_velocity = 10
-		$back.angular_velocity = 10
-	if Input.is_action_pressed("ui_left"):
-		$front.angular_velocity = -10
-		$back.angular_velocity = -10
